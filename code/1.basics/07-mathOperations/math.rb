51476f85b77b6a70a08d40a0208aaf4dca0ebc81
oluquejs <luquedevjs@icloud.com>
2025-05-16 17:00:57 -0300

    feat(ruby): 07 - math

---
+++ code/1.basics/07-mathOperations/math.rb
@@ -0,0 +1,19 @@
+# < + & - >
+puts 1 + 4
+puts 6 - 1
+
+# < * & / >
+puts 2 * 5
+puts 10 / 2
+
+## < ** & % >
+puts 2 ** 2
+puts 10 % 3
+
+# < Priority () >
+puts (1+3)*4
+puts 1+4+(5-4)
+
+# < Float Output >
+puts 12 / 5.0 # must be a float value
+puts 0.5 + 0.2
