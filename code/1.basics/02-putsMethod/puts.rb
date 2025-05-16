0ae2eeee49d51a5a05dc5294d3ac98bafc647d14
oluquejs <luquedevjs@icloud.com>
2025-05-16 16:58:59 -0300

    feat(ruby): 02 - Puts method

---
+++ code/1.basics/02-putsMethod/puts.rb
@@ -0,0 +1,25 @@
+# < Puts (output) > 
+puts 1 + 2
+
+puts "The ruby is simple in appearance, but very complex inside, like the human body."
+
+puts (3 + 4)
+
+puts "one","two","three"
+
+
+# < Scape Characters >
+puts "some text\n more text"
+
+puts "\tOnce upon a time"
+
+puts "Juliet said \"Goodbye to Romeo\"" # or
+
+puts 'Juliet said "Goodbye to Romeo"' 
+
+
+# < Concatenation>
+puts "Never gonna" + " give you up"
+
+# < Interpolation >
+puts "Balance: $#{100+50.50}"
