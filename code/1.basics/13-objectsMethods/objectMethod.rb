45c6d8f33382269285949de283d3f075e4bcdd1f
oLuqueJs <sunandmoonjs@icloud.com>
2025-05-18 19:30:07 -0300

    feat(ruby): 14 - object methods

---
+++ code/1.basics/13-objectsMethods/objectMethod.rb
@@ -0,0 +1,13 @@
+# < Object Methods >
+puts "Hello World!".length()
+
+puts "My name is Ruby!".upcase()
+
+puts "I LIKE CHEESE".downcase()
+
+
+# < Using Variables >
+
+expression = "a lot!"
+
+puts expression.upcase()
