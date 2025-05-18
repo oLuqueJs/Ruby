7879b99d15d8dc3c4a4f329b9e70943fa8fd6c4b
oLuqueJs <sunandmoonjs@icloud.com>
2025-05-17 22:57:50 -0300

    feat(ruby): 12 - assignment Operator

---
+++ code/1.basics/11-assignmentOperator/assignment.rb
@@ -0,0 +1,41 @@
+# < String Assignment ("") >
+name = "Portgas"
+name += " D. Ace"
+
+puts name
+
+# <  Sum Assignment (+=) >
+a = 10
+a += 5 # a = a + 5
+
+puts a
+
+# < Subtraction Assignment (-=) >
+b = 500
+b -= 450
+
+puts b
+
+# < Multiplication Assignment (*=) >
+c = 10
+c *= 4
+
+puts c
+
+# < Division Assignment (/=) >
+d = 20
+d /= 2
+
+puts d
+
+# < Power Assignment (**=) >
+e = 2
+e **=2
+
+puts e
+
+# < Rest Assignment (%=) >
+f = 20
+f %= 3
+
+puts f
