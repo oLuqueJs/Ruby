6ae66d6ee9d7542e40c207bee19651139e17ee1c
oluquejs <luquedevjs@icloud.com>
2025-05-19 16:50:59 -0300

    feat(ruby): 21 - type convertions

---
+++ code/1.basics/20-typeConvertion/convertions.rb
@@ -0,0 +1,17 @@
+# < String -> Integer >
+text = "5 apples"
+
+puts text.class(), text.to_i().class()
+puts text.to_i()
+
+# < Integer -> String >
+num1 = 10
+
+puts num1.class(), num1.to_s().class()
+puts num1.to_s()
+
+# < Integer -> Float >
+num2 = 244
+
+puts num2.class(), num2.to_f().class
+puts num2.to_f()
