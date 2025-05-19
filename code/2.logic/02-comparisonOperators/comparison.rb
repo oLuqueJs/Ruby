0f4ea62ddb4ce24d32772cbef230e2e33f64182e
oluquejs <luquedevjs@icloud.com>
2025-05-19 17:30:20 -0300

    feat(ruby): 23 - comparison operators

---
+++ code/2.logic/02-comparisonOperators/comparison.rb
@@ -0,0 +1,17 @@
+# < Variables >
+num1 = 10
+num2 = 5
+num3 = 10.0
+
+# < (>) & (<) >
+puts num1 > num2
+puts num1 < num2
+
+# < (>=) & (<=) >
+puts num1 >= num2
+puts num1 >= num3
+puts num3 >= num1
+
+# < (==) (!=)>
+puts num1 == num3
+puts num1 != num3
