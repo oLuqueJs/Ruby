a065d918f0e50f6641eddf21b0c78550ec7ec0d3
oluquejs <luquedevjs@icloud.com>
2025-05-20 16:09:21 -0300

    feat(ruby): 25 - methods with arguments

---
+++ code/2.logic/04-methodsArguments/arguments.rb
@@ -0,0 +1,27 @@
+# < Methods with Arguments >
+puts "Jhonny".include?("o")
+
+puts "I love Cheese".include?("Cheese")
+
+# < Methods with Multiple Arguments >
+puts 20.between?(10, 30)
+
+puts -4.between?(-9.5, 10)
+
+# < Arithmetic Methods >
+puts 5.+(23)
+
+puts 10.-(20)
+
+puts 20.div(2)
+
+# < Float Methods >
+puts 10.5.floor() # round (down)
+
+puts 10.5.ceil()  # round (closest)
+
+puts 5.35.abs()   # absolute value
+
+puts 3.14859.round(2) # 2 decimals
+
+puts -35.abs()
