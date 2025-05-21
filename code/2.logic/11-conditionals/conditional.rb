c977a0e6dcfdb2cb4b49dbcb832e2bcb5fe8deb8
oluquejs <luquedevjs@icloud.com>
2025-05-21 17:43:28 -0300

    feat(ruby): 33 - conditionals

---
+++ code/2.logic/11-conditionals/conditional.rb
@@ -0,0 +1,18 @@
+# < If Conditional >
+if 5 < 7
+  puts 'True!'
+end
+
+# < Else >
+if 5.even?
+  puts 'even'
+else
+  puts 'odd'
+end
+
+# < Elsif >
+if 5 > 10
+  puts 5
+elsif 5 < 10
+  puts 10
+end
