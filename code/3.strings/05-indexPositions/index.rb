29fc16a5094fd2def6b95cac477a66b209e54385
oLuqueJs <sunandmoonjs@icloud.com>
2025-05-25 15:02:43 -0300

    feat(ruby): 45 - index positions

---
+++ code/3.strings/05-indexPositions/index.rb
@@ -0,0 +1,14 @@
+# < String Index Positions >
+#         0123456789...
+phrase = "Stay hungry, stay foolish"
+
+puts phrase[10]
+
+
+# < Negative index >
+puts phrase[-1]
+
+
+# < Slice Method > 
+sliced = phrase.slice(-1)
+p sliced # < New String >
