f50ed161c15cb10a96018bca19e169a106c14a7a
oluquejs <luquedevjs@icloud.com>
2025-05-26 17:45:43 -0300

    feat(ruby): 49 - case methods

---
+++ code/3.strings/09-caseMethods/case.rb
@@ -0,0 +1,18 @@
+# < String >
+name = "Osaki Nana"
+
+
+# < Upcase Method >
+puts name.upcase()
+
+
+# < Downcase Method >
+puts name.downcase()
+
+
+# < Swapcase Method >
+puts name.swapcase()
+
+
+# < Capitalize Method >
+puts name.capitalize()
