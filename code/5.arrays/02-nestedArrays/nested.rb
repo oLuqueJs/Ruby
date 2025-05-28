93a0fd98ec68c6286fb577a254fbf0c27dcf69e6
oluquejs <luquedevjs@icloud.com>
2025-05-28 15:12:36 -0300

    feat(ruby): 58 nested arrays

---
+++ code/5.arrays/02-nestedArrays/nested.rb
@@ -0,0 +1,15 @@
+# < Nested Array >
+list = [
+  [1, 2, 3, 4, 5, 6],
+  ['Student', 'Class', 'Grade'],
+]
+
+p list
+
+
+# < Rows >
+first_row, second_row, third_row = list
+
+p first_row
+
+p second_row
