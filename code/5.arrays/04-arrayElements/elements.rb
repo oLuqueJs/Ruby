e347b6588ed49a59ea35279f1c10d862f0ffb793
oluquejs <luquedevjs@icloud.com>
2025-05-28 16:00:27 -0300

    feat(ruby): 60 array elements

---
+++ code/5.arrays/04-arrayElements/elements.rb
@@ -0,0 +1,17 @@
+# < Array >
+fruits = ["Apple", "Orange", "Banana", "Grape"]
+
+
+# < Array Index >
+puts fruits[0]
+puts fruits[1]
+puts fruits[2]
+puts fruits[-1]
+
+
+# < Slice >
+puts fruits.slice(0)
+
+
+# < Overwriting Values >
+fruits[1] = "Watermelon"
