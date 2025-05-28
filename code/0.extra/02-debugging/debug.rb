c6da08f0035eaf9404a564c81ff0f41b301af73c
oluquejs <luquedevjs@icloud.com>
2025-05-28 14:57:59 -0300

    feat(ruby): 56 - debug

---
+++ code/0.extra/02-debugging/debug.rb
@@ -0,0 +1,18 @@
+# < Debug Lib >
+require 'debug'
+
+candy = "Sour Patch Kids"
+puts "I love eating #{candy}}"
+
+
+debugger()
+
+
+beverage = "Arizona Iced Tea".upcase()
+puts "I love drinking #{beverage}"
+
+
+debugger()
+
+
+puts "End of the program"
