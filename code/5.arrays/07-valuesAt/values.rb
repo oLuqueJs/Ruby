69fe923ea138a6a3c9f8cb726301d6ac90044b03
oluquejs <luquedevjs@icloud.com>
2025-05-29 17:00:33 -0300

    feat(ruby): 64 values at

---
+++ code/5.arrays/07-valuesAt/values.rb
@@ -0,0 +1,10 @@
+# < Array >
+tv_channels = ["CBS", "UPN", "CW", "FOX", "NBC", "ESPN"]
+
+
+# < Values At >
+p tv_channels.values_at(0)
+
+p tv_channels.values_at(0, 4)
+
+p tv_channels.values_at(-1, -2, 4)
