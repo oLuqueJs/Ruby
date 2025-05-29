f923748518c52a9b3150b8d2863e307da4dff20d
oluquejs <luquedevjs@icloud.com>
2025-05-29 17:01:02 -0300

    feat(ruby): 66 size methods

---
+++ code/5.arrays/09-sizeMethods/size.rb
@@ -0,0 +1,15 @@
+# < Array >
+nums = [1, 2, 3, 4, 5, 10, 5]
+
+
+# < Length >
+p nums.length()
+
+
+# < Size >
+p nums.size() # alias for length
+
+
+# < Count >
+p nums.count()
+p nums.count(5) # how many times 5 appears
