ffc2bfc238bd8a9862615aaea13ff144808e3dbd
oluquejs <luquedevjs@icloud.com>
2025-05-30 16:49:49 -0300

    feat(ruby): 68 shovel operator

---
+++ code/5.arrays/11-shovel/shovel.rb
@@ -0,0 +1,11 @@
+# < Array >
+soups = ["French Onion", "Clam CHowder", "Chicken Noodle"]
+
+
+# < Push Method >
+soups.push("Miso")
+
+soups.push("Hot and Sour", "Potato Soup")
+
+# < Shovel Operator >
+soups << "Wonton"
