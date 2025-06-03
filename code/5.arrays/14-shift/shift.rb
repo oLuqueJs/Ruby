7b17ef2b7abaf97d2b5561c0038c8fc65cb55a55
oluquejs <luquedevjs@icloud.com>
2025-06-03 11:01:53 -0300

    feat(ruby): 71 unshift & shift

---
+++ code/5.arrays/14-shift/shift.rb
@@ -0,0 +1,15 @@
+# < Array >
+units = ["meter", "kilogram", "second", "ampere"]
+
+
+# < Shift >
+p units.shift()
+
+removed_items = units.shift(2)
+p removed_items
+
+p units
+
+
+# < UnShift >
+p units.unshift("yard")
