d73fc5f47ab152af3bc90a64d55a3e4297a715c3
oluquejs <luquedevjs@icloud.com>
2025-06-03 17:03:44 -0300

    feat(ruby): 80 for

---
+++ code/7.arraysII/03-for/for.rb
@@ -0,0 +1,18 @@
+# < Array >
+nums = [1, 2, 3]
+
+
+# < For Loop >
+for number in nums
+  p number
+end
+
+puts number # number persist....
+
+
+# < For + Range >
+for value in 1..10
+  puts value
+end
+
+puts value
