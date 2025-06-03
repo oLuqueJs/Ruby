4370b3d2aa996f96998f9a3e2a23c9f937e581d1
oluquejs <luquedevjs@icloud.com>
2025-06-03 11:01:29 -0300

    feat(ruby): 70 pop

---
+++ code/5.arrays/13-pop/pop.rb
@@ -0,0 +1,14 @@
+# < Array >
+bubble_tea = [
+  "Chocolate", "Kiwi", "Peach", "Plum", "Strawberry", "Taro"
+]
+
+p bubble_tea
+
+
+# < Pop Method >
+p bubble_tea.pop(1)
+
+removed_item = bubble_tea.pop(2)
+
+p removed_item
