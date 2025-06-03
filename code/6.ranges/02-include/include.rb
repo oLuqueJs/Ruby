8137c35fb95e9f95bbde3ff2486ffe996b18a7b8
oluquejs <luquedevjs@icloud.com>
2025-06-03 15:29:08 -0300

    feat(ruby): 73 include & range

---
+++ code/6.ranges/02-include/include.rb
@@ -0,0 +1,14 @@
+# < Range >
+alphabet = 'a'..'z'
+
+# < Include >
+puts alphabet.include?('J')
+puts alphabet.include?('Z')
+
+# < Member >
+puts alphabet.member?('j')
+puts alphabet.member?('O')
+
+# < === >
+puts alphabet === 'k'
+puts alphabet === 'O'
