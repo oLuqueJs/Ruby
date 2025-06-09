d556759e556d8e6fb4848b43875a3a488fd5e57a
oluquejs <luquedevjs@icloud.com>
2025-06-09 11:58:42 -0300

    feat(rubt): 82 iteration

---
+++ code/7.arraysII/05-iteration/iteration.rb
@@ -0,0 +1,23 @@
+# < Array >
+animals = ["Lion", "Zebra", "Baboon", "Cheetah"]
+
+
+# < Index >
+i = 0
+
+
+# < While >
+while i < animals.length
+  puts animals[i]
+  i += 1;
+end
+
+
+i = 0
+
+
+# < Until >
+until i == animals.length
+  puts animals[i]
+  i += 1
+end
